fca3e3cbc04950f9b25002dc04c222690876dc6d
Markus Siegert <spongebobmarkus@gmail.com>
2025-05-14 11:40:20 +0200

    ci: memo integration

--- memo-api/main.py
+++ memo-api/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from memo-api!")
+from memo import inference
+from importlib import resources
 
+def get_resource_path(dir: str, file: str) -> str:
+    with resources.path(f"memo.{dir}", file) as p:
+        return str(p)
+
+def main():
+    # --config memo/configs/inference.yaml
+    # --input_image memo/assets/examples/dicaprio.jpg
+    # --input_audio memo/assets/examples/speech.wav --output_dir outputs
+
+    args = [
+        "--config", get_resource_path("configs", "inference.yaml"),
+        "--input_image", get_resource_path("assets.examples", "dicaprio.jpg"),
+        "--input_audio", get_resource_path("assets.examples", "speech.wav"),
+        "--output_dir", "/tmp/out",
+        # "--seed", "1234",
+    ]
+
+    inference.main(args)
 
 if __name__ == "__main__":
     main()
